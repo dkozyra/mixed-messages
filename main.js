fcd0f76a6d544d0d1c7632826059703a49867d2a
Damian Kozyra <d.kozyra@hotmail.co.uk>
2021-10-09 16:25:16 +0100

    most functionality implemented

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('let start it');
+let randGen = (x) => {
+    return Math.floor(Math.random() * x);
+}
+
+let factsAboutYou = [];
+
+let personality = {
+    appearance: ['fat', 'toll', 'ugly', 'bauty', 'skinny', 'short'],
+    home: ['tree', 'bin', 'hotel', 'basement', 'superstore', 'apartament'],
+    activity: ['football', 'tennis', 'biking', 'mud swimming', 'eating', 'karate']
+}
+
+for(let prop in personality){
+    switch(prop){
+        case 'appearance':
+            factsAboutYou.push(`You are really ${prop[randGen(prop.length)]} and ${prop[randGen(prop.length)]} person`);
+            break;
+        case 'home':
+            factsAboutYou.push(`The place you usually live is: ${prop[randGen(prop.length)]} or ${prop[randGen(prop.length)]}. It is really nice place to live`);
+            break;
+        case 'activity':
+            factsAboutYou.push(`Your favorite activity is: ${prop[randGen(prop.length)]}, alternatively you also like ${prop[randGen(prop.length)]}`);
+            break;
+    }
+}
+
+console.log(factsAboutYou);
