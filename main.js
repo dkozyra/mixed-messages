af07c0030376893b79bc46a0d139b065f382fa7f
Damian Kozyra <d.kozyra@hotmail.co.uk>
2021-10-09 19:09:48 +0100

    pre final

--- main.js
+++ main.js
@@ -2,26 +2,28 @@ let randGen = (x) => {
     return Math.floor(Math.random() * x);
 }
 
-let factsAboutYou = [];
+let factsAboutYouArray = [];
 
 let personality = {
-    appearance: ['fat', 'toll', 'ugly', 'bauty', 'skinny', 'short'],
-    home: ['tree', 'bin', 'hotel', 'basement', 'superstore', 'apartament'],
-    activity: ['football', 'tennis', 'biking', 'mud swimming', 'eating', 'karate']
+    appearance: ['fat', 'tall', 'ugly', 'bauty', 'skinny', 'short','small', 'huge', 'funny', 'smart', 'grumpy', 'positive', 'egoistic'],
+    home: ['tree', 'bin', 'hotel', 'basement', 'superstore', 'apartament', 'car', 'wood', 'forest', 'roadside', 'mountains', 'ocean', 'lake'],
+    activity: ['football', 'tennis', 'biking', 'mud swimming', 'eating', 'karate', 'barking', 'cleaning', 'watching anime', 'hoovering', 'flying']
 }
 
 for(let prop in personality){
     switch(prop){
         case 'appearance':
-            factsAboutYou.push(`You are really ${prop[randGen(prop.length)]} and ${prop[randGen(prop.length)]} person`);
+            factsAboutYouArray.push(`You are really ${personality[prop][randGen(personality[prop].length)]} and ${personality[prop][randGen(personality[prop].length)]} person. `);
             break;
         case 'home':
-            factsAboutYou.push(`The place you usually live is: ${prop[randGen(prop.length)]} or ${prop[randGen(prop.length)]}. It is really nice place to live`);
+            factsAboutYouArray.push(`The place you usually live is: ${personality[prop][randGen(personality[prop].length)]} or ${personality[prop][randGen(personality[prop].length)]}. It is really nice place to live. `);
             break;
         case 'activity':
-            factsAboutYou.push(`Your favorite activity is: ${prop[randGen(prop.length)]}, alternatively you also like ${prop[randGen(prop.length)]}`);
+            factsAboutYouArray.push(`Your favorite activity is: ${personality[prop][randGen(personality[prop].length)]}, alternatively you also like ${personality[prop][randGen(personality[prop].length)]}. `);
             break;
     }
 }
 
-console.log(factsAboutYou);
+let message = factsAboutYouArray.toString().replace(/,/g, '');
+
+console.log(message);
